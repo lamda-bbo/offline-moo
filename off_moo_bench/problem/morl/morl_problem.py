160ec01b806227e29f74ba29e09f8967d3813854
trxcc <rongxitan@smail.nju.edu.cn>
2024-09-18 01:04:35 +0800

    Fix bugs in MORL nadir point

--- off_moo_bench/problem/morl/morl_problem.py
+++ off_moo_bench/problem/morl/morl_problem.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class MOHopperV2(MORLProblem):
     def __init__(self, n_obj=2, env_name="MO-Hopper-v2"):
-        nadir_point = np.array([154.8246919, 1681.81775345])
+        nadir_point = np.array([154.8246919, 1681.81775345]) * 2
         ideal_point = np.array([-1179.36239177, -1370.84501144])
         
         super().__init__(
@@ -16,7 +16,7 @@ class MOHopperV2(MORLProblem):
 
 class MOSwimmerV2(MORLProblem):
     def __init__(self, n_obj=2, env_name="MO-Swimmer-v2"):
-        nadir_point = np.array([ 24.00957411, -25.47507349])
+        nadir_point = np.array([ 24.00957411, -25.47507349]) * 2
         ideal_point = np.array([-219.65312991, -149.99974259])
         
         super().__init__(
